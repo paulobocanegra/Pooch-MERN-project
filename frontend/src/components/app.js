03fd3f4a5adbb56e475ade03c4d5886367707a18
Omar Hernandez <oah232@gmail.com>
2020-10-26 15:11:04 -0700

    minor changes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,6 +5,7 @@ import { Switch } from "react-router-dom";
 import MainPageContainer from './main/main_page_container';
 import LoginFormContainer from "./session/login_form_container";
 import RegisterFormContainer from "./session/register_form_container";
+import LandingPageContainer from './landing/landing_container'
 
 
 const App = () => (
@@ -14,6 +15,7 @@ const App = () => (
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/register" component={RegisterFormContainer} />
       <ProtectedRoute exact path="/" component={MainPageContainer} />
+      <Route path='/landing' component={LandingPageContainer} />
     </Switch>
   </div>
 );
