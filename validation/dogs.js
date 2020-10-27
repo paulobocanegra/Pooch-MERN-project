8e85cd01d1d17d5f386af37859a5a9d8d9cc19e4
paulobocanegra <paulobocanegra@gmail.com>
2020-10-27 14:31:37 -0700

    validations

--- validation/dogs.js
+++ validation/dogs.js
@@ -13,13 +13,38 @@ module.exports = function validateDogInput(data) {
   // bio: req.body.bio,
 
     data.name = validText(data.name) ? data.name : '';
+    // data.age = validText(data.age) ? data.age : '';
+    data.breed = validText(data.breed) ? data.breed : '';
+    data.sex = validText(data.sex) ? data.sex : '';
+    data.size = validText(data.size) ? data.size : '';
+    data.bio = validText(data.bio) ? data.bio : '';
 
-    if (!Validator.isLength(data.name, { min: 1, max: 140 })) {
-        errors.name = 'Dog name must be between 5 and 140 characters';
+    if (!Validator.isLength(data.name, { min: 1, max: 30 })) {
+        errors.name = 'Dog name must be between 1 and 30 characters';
     }
 
     if (Validator.isEmpty(data.name)) {
         errors.name = 'Name field is required';
+    }
+
+    if (!Validator.isLength(data.breed, { min: 1, max: 30 })) {
+        errors.breed = 'Dog breed must be between 1 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.breed)) {
+        errors.breed = 'Breed field is required';
+    }
+
+    if (Validator.isEmpty(data.sex)) {
+        errors.sex = 'Sex field is required';
+    }
+
+    if (Validator.isEmpty(data.size)) {
+        errors.size = 'Size field is required';
+    }
+
+    if (Validator.isEmpty(data.bio)) {
+        errors.bio = 'Bio field is required';
     }
 
     return {
