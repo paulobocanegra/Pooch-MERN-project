f9f908491df6b2e143a23609fd38b152913a5449
diego <diegorenechavez@gmail.com>
2020-10-28 14:36:12 -0700

    Log in and Sign in form

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import session from "./session/session_reducer";
 import errors from "./session/errors_reducer";
 import ui from "./ui_reducers";
+import EntitiesReducer from "./entities_reducer";
 
 const RootReducer = combineReducers({
+  entities: EntitiesReducer,
   errors,
   session,
   ui
---
+++ frontend/src/reducers/users_reducer.js
@@ -0,0 +1,16 @@
+import {RECEIVE_CURRENT_USER} from "../actions/session_actions";
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  // debugger
+  switch (action.type) {
+    // debugger
+    case RECEIVE_CURRENT_USER:
+      newState[action.user.id] = action.user;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
