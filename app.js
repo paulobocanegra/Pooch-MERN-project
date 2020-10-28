26d3cb1c9b6dba9e8d58759220e9980db50e7216
Omar Hernandez <oah232@gmail.com>
2020-10-28 13:57:39 -0700

    fixed config for heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
+const path = require('path');
 
 /** */
 const multer = require("multer");
@@ -13,6 +14,12 @@ var AWS = require("aws-sdk");
 
 const dogs = require("./routes/api/dogs")
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
 .connect(db, { useNewUrlParser: true })
