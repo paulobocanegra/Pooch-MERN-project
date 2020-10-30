fb258bdd3d3e8a289bb09403837bfca867c4322b
Juan <juancattaneo92@gmail.com>
2020-10-30 13:44:26 -0700

    frontend feed

--- frontend/src/components/feed/feed.jsx
+++ frontend/src/components/feed/feed.jsx
@@ -9,9 +9,16 @@ class Feed extends React.Component{
 
     render(){
         return (
-          <div className="Discover-center">
+          <div className="main-discover-center">
+            <div className="box-discover">
+              
             <div className="Message-center">
+              <div className="Message-innerbox">
+                <div className="User-avatar-container">
               <img className="User-avatar" src="./empty_profile.png" alt="" />
+        <p className="name">{this.props.currentUser.name}</p>
+                    <div className="user-icon"><i class="fas fa-user"></i></div>
+                </div>
               <input
                 className="Search-matches"
                 type="search"
@@ -20,12 +27,37 @@ class Feed extends React.Component{
               <ul>
                 <div className="Matched-user">
                 <img className="User-avatar" src="./empty_profile.png" alt="" />
-                <p className="new">New Play Date!</p>
+                <p className="new">Name</p>
                 </div>
               </ul>
+              </div>
             </div>
-            <div className="Discover-nav">
-                <button onClick={ this.props.logOut}>Logout</button>
+            <div className="rightSide-container">
+              <div className="Discover-nav">
+                  {/* <Link to="/"> */}
+                  <div className="discover-nav-left">
+                    <img className="Logo" src="./Pooch-logo.png" alt="" />
+                    <h1 className="Logo-text">pooch</h1> 
+                  </div>
+                  {/* </Link> */}
+
+                  <div className="discover-nav-right">
+                    
+        <h3 className="Nav-bar-welcome">Hello, {this.props.currentUser.name}</h3>
+
+                  <div className="Nav-button2" onClick={this.props.logout}>Logout</div>
+                  </div>
+              </div>
+
+                <div className="picture-bio-container">
+                  <div className="picture-bio-inner-container"> 
+                    <div className="picture-container"></div>
+                    <div className="bio-container"></div>
+                  </div>
+                </div>
+            </div>
+
+
             </div>
             
           </div>
