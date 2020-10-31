a0388aae10715a434a4a24245766cb1729e2ae76
paulobocanegra <paulobocanegra@gmail.com>
2020-10-31 13:01:18 -0700

    updated api util and action for user

--- frontend/src/util/user_util.js
+++ frontend/src/util/user_util.js
@@ -4,6 +4,10 @@ export const fetchUsers = () => (
    axios.get(`/api/users`)
 );
 
+export const fetchUser = (userId) => (
+   axios.get(`/api/users/${userId}`)
+);
+
 export const updateUser = (userId, data) => (
    axios.patch(`api/users/${userId}`, data)
 );
