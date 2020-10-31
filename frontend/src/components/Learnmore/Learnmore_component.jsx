f5e59f250eba3e0aedfc6bc9345e8a881364046f
diego <diegorenechavez@gmail.com>
2020-10-31 15:50:47 -0700

    learn-more-modal

---
+++ frontend/src/components/Learnmore/Learnmore_component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Learnmore extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1>Pooch can open up a whole other world for you and your pup</h1>
+            </div>
+        )
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+export default Learnmore;
