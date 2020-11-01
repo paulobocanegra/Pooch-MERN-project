2c135d99718edfbfe73784d667f7178ed40d65b3
Juan <juancattaneo92@gmail.com>
2020-10-31 17:11:41 -0700

    Git conflicts fix

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -5,25 +5,27 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer-div">
-                <div>
+
+                    <div className="logo-footer">
+                        <img className="Logo" src="./Pooch-logo.png" alt="" />
+                        <h1 className="Logo-text-footer">pooch</h1>
+                    </div>
                     <div className="footer-section">
-                        <div className="footer-ul">
+                        <div className="footer-left">
                             <h3 className="footer-title">About</h3>
                             <ul>
-                                <li className="footer-li"><a target="_blank" className="footer-link" href="https://github.com/juancattaneo92/Pooch-MERN-project">About Pooch</a></li>
+                                <li className="footer-li"><a target="_blank" className="footer-link" href="https://github.com/juancattaneo92/Pooch-MERN-project">Pooch</a></li>
                                 <li className="footer-li"><a target="_blank" className="footer-link" href="https://www.linkedin.com/in/paulo-bocanegra-a89b641b9/">Developers</a></li>
                             </ul>
                         </div>
-                        <div className="footer-ul">
+                        <div className="footer-right">
                             <h3 className="footer-title">Contact</h3>
                             <ul className="contact-text">
-                                <li className="footer-li">Email: pooch@gmail.com</li>
-                                <li className="footer-li"></li>
+                            <li className="footer-li"><a target="_blank" className="footer-link" href="https://www.gmail.com">Email: pooch@gmail.com</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
-            </div>
         )
     }
 }
